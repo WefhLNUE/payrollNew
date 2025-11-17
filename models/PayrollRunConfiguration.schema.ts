9d3d9a12bcd3e8e5c017d93ede4b7390bbaeefb3
Aster-00 <ammarabdelkhalek021@gmail.com>
2025-11-17 16:34:14 +0200

    Fix import statement formatting in PayrollRunConfiguration.schema.ts

--- models/PayrollRunConfiguration.schema.ts
+++ models/PayrollRunConfiguration.schema.ts
@@ -1,4 +1,4 @@
-import import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 @Schema({ timestamps: true })
