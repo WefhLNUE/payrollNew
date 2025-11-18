c2e11b134d0e631cd1eaa686b93085e07be090e2
Aster-00 <ammarabdelkhalek021@gmail.com>
2025-11-18 16:03:07 +0200

    Update hr-recruitment submodule to latest commit b50b87f and add new dependencies in package.json and package-lock.json for @nestjs/config, @nestjs/mapped-types, axios, class-transformer, and class-validator.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HrRecruitmentModule } from '../hr-recruitment/hr-recruitment.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+    HrRecruitmentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
